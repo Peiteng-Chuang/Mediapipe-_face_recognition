0e72b2441ca5be7322a2ce8d03f6dfac06d53744
peiteng <peiteng.chuang@gmail.com>
2024-06-21 12:28:31 +0800

    qw

--- test_mediapipe/do_a_csv_test.py
+++ test_mediapipe/do_a_csv_test.py
@@ -50,11 +50,12 @@ lip=[146,91,181,84,17,314,
 lip_u=[0,37,39,40,185,191,80,81,82,13,312,311,310,267,269,270]
 
 ford_head=[54,284]
+width=[234,454]
 low_jaw=[172,397]
-jaw=[234,454]
 long=[175,10]
 nose=[4]
-ff=long+jaw+low_jaw+ford_head+nose
+all_points=[ford_head,width,low_jaw,long]
+ff=long+width+low_jaw+ford_head+nose
 # FEATURE_INDEXES = [16]
 FEATURE_INDEXES = ff
 
@@ -63,11 +64,7 @@ image_path = './test_mediapipe/test_image/max.jpg'  # 替换为你的图片路�
 # image_path = './test_mediapipe/test_image/amy.jpg'  # 替换为你的图片路径
 # image_path = './test_mediapipe/256img_lst/001.jpg'  # 替换为你的图片路径
 
-def get_xyz(landmark):
-    x=landmark.x
-    y=landmark.y
-    z=landmark.z
-    return x,y,z
+
 
 def get_distance():
     return 0
@@ -75,15 +72,46 @@ def get_distance():
 image = cv2.imread(image_path)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 results = face_mesh.process(image_rgb)
+distance_array=[0,0,0,0]
+WD,HG=image.shape[1],image.shape[0]
 
 if results.multi_face_landmarks:
+    
     landmarks = results.multi_face_landmarks[0].landmark
-    for idx in FEATURE_INDEXES:
-        x = int(landmarks[idx].x * image.shape[1])
-        y = int(landmarks[idx].y * image.shape[0])
-        cv2.circle(image, (x, y), 2, (0, 0, 255), -1)  # 用绿色绘制特征点
-        cv2.putText(image, f'{idx}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-        print(f"{idx}_xyz :x={landmarks[idx].x}/y={landmarks[idx].y}/z={landmarks[idx].z}")
+    def get_xyz(inpt):
+        x,y,z=landmarks[inpt].x,landmarks[inpt].y,landmarks[inpt].z
+        return x,y,z
+    def get_distance(point1, point2):
+        p1,p2=landmarks[point1],landmarks[point2]
+        return np.linalg.norm(np.array([p1.x, p1.y]) - np.array([p2.x, p2.y]))
+    def get_imxy(number):
+        imx = int(landmarks[number].x * WD)
+        imy = int(landmarks[number].y * HG)
+        return imx,imy
+    for id,point in enumerate(all_points,start=1):
+        point1 = get_imxy(point[0])
+        point2 = get_imxy(point[1])
+        
+        for inpt in point:
+            x,y=get_imxy(inpt)
+            mpx,mpy,mpz=get_xyz(inpt)
+            cv2.circle(image, (x, y), 3, (0, 255, 0), -1)  # 用绿色绘制特征点
+            cv2.putText(image, f'{inpt}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            
+            print(f"{inpt}_loc :\tx={mpx} \ty={mpy} \tz={mpz}")
+        
+        temp=get_distance(point[0],point[1])
+        
+        cv2.line(image, point1, point2, (0, 255, 0), 1)
+        cv2.putText(image, f'line{id} : {temp:.3f}', (20, 35*id), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        
+        tag_loc=(get_imxy(point[1]))
+        tag_loc=tag_loc[0],tag_loc[1]-20
+        cv2.putText(image, f'line{id}', tag_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        
+        distance_array[id-1]=temp
+    for id,a in enumerate(distance_array):
+        print(id+1,a)
     
     
 # 顯示圖片
